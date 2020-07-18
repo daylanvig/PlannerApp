e4e7c490ab77e571d89aaf759ecb77f2c9105dc6
daylanvig <daylanvig@gmail.com>
2020-07-18 09:49:17 -0600

    Add swipe event to change dates

--- PlannerApp/Components/WeekCalendarBase.cs
+++ PlannerApp/Components/WeekCalendarBase.cs
@@ -12,11 +12,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using UIComponents.Bulma;
 using UIComponents.Bulma.Helpers;
+using UIComponents.Extensions;
 using UIComponents.Services;
 
 namespace PlannerApp.Client.Components
 {
-    public class WeekCalendarBase : ComponentBase
+    public class WeekCalendarBase : ComponentBase, ISwipeEventSubscriber
     {
         [Inject]
         public AppState AppState { get; set; }
@@ -30,30 +31,41 @@ namespace PlannerApp.Client.Components
         public ICategoryDataService CategoryDataService { get; set; }
         [Inject]
         public IModalService ModalService { get; set; }
+        protected readonly TouchSwipeEvent SwipeEvent = new TouchSwipeEvent();
         public DateTime ViewingWeekOf = DateTimeHelper.GetMostRecentDayOfWeek(DateTime.Today, DayOfWeek.Sunday);
-
         protected ICollection<PlannerItemDTO> Items = new List<PlannerItemDTO>();
 
-        private IEnumerable<CategoryDTO> categories;
         protected List<DateTime> ViewingDates
         {
             get => GetViewingDates();
         }
-        private List<DateTime> GetViewingDates()
-        {
-            var dates = new List<DateTime>();
-            for (var i = 0; i < 7; i++)
-            {
-                dates.Add(ViewingWeekOf.AddDays(i));
-            }
-            return dates;
-        }
-
+      
         protected override async Task OnInitializedAsync()
         {
             SetTitle();
             Items = (await PlannerItemDataService.LoadItems(ViewingWeekOf, ViewingWeekOf.AddDays(7))).ToList();
-            categories = await CategoryDataService.LoadCategories();
+            SwipeEvent.Subscribe(this);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && DateTime.Now.Hour != 0)
+            {
+                // scroll to display current time
+                await DOMService.ScrollIntoView($"#interval-{DateTime.Now.Hour - 1}");
+            }
+        }
+
+        public void HandleSwipe(SwipeDirection direction)
+        {
+            if(direction == SwipeDirection.Left)
+            {
+                ViewingWeekOf = ViewingWeekOf.AddDays(7);
+            }
+            else
+            {
+                ViewingWeekOf = ViewingWeekOf.AddDays(-7);
+            }
         }
 
         private void SetTitle()
@@ -65,13 +77,15 @@ namespace PlannerApp.Client.Components
                 ));
         }
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+        private List<DateTime> GetViewingDates()
         {
-            if (firstRender && DateTime.Now.Hour != 0)
+            var dates = new List<DateTime>();
+            for (var i = 0; i < 7; i++)
             {
-                // scroll to display current time
-                await DOMService.ScrollIntoView($"#interval-{DateTime.Now.Hour - 1}");
+                dates.Add(ViewingWeekOf.AddDays(i));
             }
+            return dates;
         }
+
     }
 }
---
+++ UIComponents/Extensions/TouchSwipeEvent.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIComponents.Extensions
+{
+    public enum SwipeDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
+    public interface ISwipeEventSubscriber
+    {
+        void HandleSwipe(SwipeDirection direction);
+    }
+
+    public class TouchSwipeEvent
+    {
+        private const int HOLD_TIMEOUT = 250; // ms
+        private readonly List<ISwipeEventSubscriber> subscribers = new List<ISwipeEventSubscriber>();
+        private DateTime? touchStartTime;
+        private double? initialX;
+        private double? initialY;
+        private bool isFirstMove;
+
+        public void Subscribe(ISwipeEventSubscriber subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+
+        public void UnSubscribe(ISwipeEventSubscriber subscriber)
+        {
+            subscribers.Remove(subscriber);
+        }
+
+        private void NotifySubscribers(SwipeDirection direction)
+        {
+            foreach(var subscriber in subscribers)
+            {
+                subscriber.HandleSwipe(direction);
+            }
+        }
+
+        public void HandleTouchStart(TouchEventArgs e)
+        {
+            touchStartTime = DateTime.Now;
+            initialX = e.Touches[0].ClientX;
+            initialY = e.Touches[0].ClientY;
+            isFirstMove = true;
+        }
+
+        public void HandleTouchMove(TouchEventArgs e)
+        {
+            // if the value is null, there has already been a move event fired
+            if (!touchStartTime.HasValue)
+            {
+                return;
+            }
+            if (isFirstMove && IsPressAndHold())
+            {
+                // No action should be taken if the user press and held without moving
+                // todo -> debug code
+                Console.WriteLine("press and hold");
+            }
+            else
+            {
+                SwipeDirection direction;
+                var newX = e.Touches[0].ClientX;
+                var newY = e.Touches[0].ClientY;
+                var diffX = initialX.Value - newX;
+                var diffY = initialY.Value - newY;
+                // is horizontal swipe
+                if (Math.Abs(diffX) > Math.Abs(diffY))
+                {
+                    direction = diffX > 0 ? SwipeDirection.Left : SwipeDirection.Right;
+                }
+                else
+                {
+                    direction = diffY > 0 ? SwipeDirection.Up : SwipeDirection.Down;
+                }
+                touchStartTime = null;
+                initialX = null;
+                initialY = null;
+                NotifySubscribers(direction);
+            }
+        }
+
+        private bool IsPressAndHold()
+        {
+            return (DateTime.Now - touchStartTime.Value).TotalMilliseconds > HOLD_TIMEOUT;
+        }
+    }
+}
