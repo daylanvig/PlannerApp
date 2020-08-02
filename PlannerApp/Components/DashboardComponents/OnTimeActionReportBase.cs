7faa883104ee3d72d83360c8d1162dc0d79dc48c
daylanvig <daylanvig@gmail.com>
2020-08-02 09:57:53 -0600

    Add basic dashboard card format.

---
+++ PlannerApp/Components/DashboardComponents/OnTimeActionReportBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Client.Services;
+using PlannerApp.Shared.Common;
+using PlannerApp.Shared.Models;
+using PlannerApp.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Client.Components.DashboardComponents
+{
+    public class OnTimeActionReportBase : ComponentBase
+    {
+        [Inject] IPlannerItemDataService PlannerItemDataService { get; set; }
+        [Inject] IDateTimeProvider DateTimeProvider { get; set; }
+        protected ICollection<PlannerItemDTO> OverdueItems;
+
+        protected override async Task OnInitializedAsync()
+        {
+            OverdueItems = (await PlannerItemDataService.LoadOverdueItems()).ToList();
+        }
+
+        protected string GetDaysOverdue(PlannerItemDTO item)
+        {
+            var daysOverdue = DateTimeHelper.DaysBetween(DateTimeProvider.Now, item.PlannedActionDate.Value);
+            // less than a day, display hours
+            if(daysOverdue < 1)
+            {
+                return $"{Math.Truncate(daysOverdue * 24)} hours";
+            }
+            return $"{Math.Truncate(daysOverdue)} days";
+        }
+    }
+}
---
+++ PlannerApp/Components/DashboardComponents/TimeSpentByCategoryReportBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Client.Services;
+using PlannerApp.Shared.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Client.Components.DashboardComponents
+{
+    public class TimeSpentByCategoryReportBase : ComponentBase
+    {
+        [Inject] ICategoryService CategoryService { get; set; }
+
+        protected IDictionary<CategoryDTO, int> CategoryTimes;
+        protected int MaxTime;
+        protected override async Task OnInitializedAsync()
+        {
+            CategoryTimes = await CategoryService.GetTotalMinutesByCategory();
+            MaxTime = CategoryTimes.Max(c => c.Value);
+        }
+
+        protected string CalculatePercentTotalWidth(int itemTime)
+        {
+            var relativeToMax = (double)itemTime / MaxTime;
+            return $"{relativeToMax * 100}%";
+        }
+    }
+}
